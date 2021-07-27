5dd0fe3945864d4c276370f47f2ff35359029ac3
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-07-27 07:20:46 +0200

    Refactor to SvgConverter

---
+++ SvgToXaml/SvgConverter.cs
@@ -0,0 +1,194 @@
+﻿using System.Globalization;
+using System.Text;
+using ShimSkiaSharp;
+
+namespace SvgToXaml
+{
+    public static class SvgConverter
+    { 
+        public static void ToPathData(SKPath path, StringBuilder sb)
+        {
+            if (path.FillType != SKPathFillType.Winding)
+            {
+                sb.Append($"F1 ");
+            }
+
+            if (path.Commands is null)
+            {
+                return;
+            }
+
+            for (var index = 0; index < path.Commands.Count; index++)
+            {
+                var pathCommand = path.Commands[index];
+
+                switch (pathCommand)
+                {
+                    case MoveToPathCommand(var x, var y):
+                    {
+                        sb.Append($"{(index > 0 ? " " : "")}M{x.ToString(CultureInfo.InvariantCulture)},{y.ToString(CultureInfo.InvariantCulture)}");
+                        break;
+                    }
+                    case LineToPathCommand(var x, var y):
+                    {
+                        sb.Append($"{(index > 0 ? " " : "")}L{x.ToString(CultureInfo.InvariantCulture)},{y.ToString(CultureInfo.InvariantCulture)}");
+                        break;
+                    }
+                    case ArcToPathCommand(var rx, var ry, var xAxisRotate, var largeArc, var sweep, var x, var y):
+                    {
+                        sb.Append($"{(index > 0 ? " " : "")}A{rx.ToString(CultureInfo.InvariantCulture)},{ry.ToString(CultureInfo.InvariantCulture)} {xAxisRotate.ToString(CultureInfo.InvariantCulture)}, {(largeArc == SKPathArcSize.Large ? "1" : "0")}, {(sweep == SKPathDirection.Clockwise ? "1" : "0")}, {x.ToString(CultureInfo.InvariantCulture)},{y.ToString(CultureInfo.InvariantCulture)});");
+                        break;
+                    }
+                    case QuadToPathCommand(var x0, var y0, var x1, var y1):
+                    {
+                        sb.Append($"{(index > 0 ? " " : "")}Q{x0.ToString(CultureInfo.InvariantCulture)},{y0.ToString(CultureInfo.InvariantCulture)} {x1.ToString(CultureInfo.InvariantCulture)},{y1.ToString(CultureInfo.InvariantCulture)}");
+                        break;
+                    }
+                    case CubicToPathCommand(var x0, var y0, var x1, var y1, var x2, var y2):
+                    {
+                        sb.Append($"{(index > 0 ? " " : "")}C{x0.ToString(CultureInfo.InvariantCulture)},{y0.ToString(CultureInfo.InvariantCulture)} {x1.ToString(CultureInfo.InvariantCulture)},{y1.ToString(CultureInfo.InvariantCulture)} {x2.ToString(CultureInfo.InvariantCulture)},{y2.ToString(CultureInfo.InvariantCulture)}");
+                        break;
+                    }
+                    case ClosePathCommand:
+                    {
+                        sb.Append($" Z");
+                    }
+                        break;
+                    case AddRectPathCommand(var skRect):
+                    {
+                        // TODO:
+                        break;
+                    }
+                    case AddRoundRectPathCommand(var skRect, var rx, var ry):
+                    {
+                        // TODO:
+                        break;
+                    }
+                    case AddOvalPathCommand(var skRect):
+                    {
+                        // TODO:
+                        break;
+                    }
+                    case AddCirclePathCommand(var f, var y, var radius):
+                    {
+                        // TODO:
+                        break;
+                    }
+                    case AddPolyPathCommand(var skPoints, var close):
+                    {
+                        if (skPoints is not null && skPoints.Count >= 2)
+                        {
+                            var mx = skPoints[0].X;
+                            var my = skPoints[0].Y;
+                            
+                            sb.Append($"{(index > 0 ? " " : "")}M{mx.ToString(CultureInfo.InvariantCulture)},{my.ToString(CultureInfo.InvariantCulture)}");
+
+                            for (int i = 1; i < skPoints.Count; i++)
+                            {
+                                var lx = skPoints[i].X;
+                                var ly = skPoints[i].Y;
+                                sb.Append($"{(index > 0 ? " " : "")}L{lx.ToString(CultureInfo.InvariantCulture)},{ly.ToString(CultureInfo.InvariantCulture)}");
+                            }
+
+                            if (close)
+                            {
+                                sb.Append($" Z");
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
+        public static string ToXaml(SKPicture? model)
+        {
+            var sb = new StringBuilder();
+
+            sb.Append($"<DrawingGroup>\r\n");
+
+            if (model?.Commands is { })
+            {
+                foreach (var canvasCommand in model.Commands)
+                {
+                    switch (canvasCommand)
+                    {
+                        case ClipPathCanvasCommand(var clipPath, var skClipOperation, var antialias):
+                        {
+                            // TODO:
+                            break;
+                        }
+                        case ClipRectCanvasCommand(var skRect, var skClipOperation, var antialias):
+                        {
+                            // TODO:
+                            break;
+                        }
+                        case SaveCanvasCommand:
+                        {
+                            // TODO:
+                            break;
+                        }
+                        case RestoreCanvasCommand:
+                        {
+                            // TODO:
+                            break;
+                        }
+                        case SetMatrixCanvasCommand(var skMatrix):
+                        {
+                            // TODO:
+                            break;
+                        }
+                        case SaveLayerCanvasCommand(var count, var skPaint):
+                        {
+                            // TODO:
+                            break;
+                        }
+                        case DrawImageCanvasCommand(var skImage, var skRect, var dest, var skPaint):
+                        {
+                            // TODO:
+                            break;
+                        }
+                        case DrawPathCanvasCommand(var skPath, var skPaint):
+                        {
+                            var sbPath = new StringBuilder();
+
+                            ToPathData(skPath, sbPath);
+
+                            var data = sbPath.ToString();
+
+                            if (skPaint.Style == SKPaintStyle.Fill)
+                            {
+                                if (skPaint.Shader is ColorShader colorShader)
+                                {
+                                    var brush =
+                                        $"#{colorShader.Color.Alpha:X2}{colorShader.Color.Red:X2}{colorShader.Color.Green:X2}{colorShader.Color.Blue:X2}";
+                                    sb.Append($"  <GeometryDrawing Brush=\"{brush}\" Geometry=\"{data}\"/>\r\n");
+                                }
+                            }
+                            break;
+                        }
+                        case DrawTextBlobCanvasCommand(var skTextBlob, var f, var y, var skPaint):
+                        {
+                            // TODO:
+                            break;
+                        }
+                        case DrawTextCanvasCommand(var text, var f, var y, var skPaint):
+                        {
+                            // TODO:
+                            break;
+                        }
+                        case DrawTextOnPathCanvasCommand(var text, var skPath, var hOffset, var vOffset, var skPaint):
+                        {
+                            // TODO:
+                            break;
+                        }
+                    }
+                }
+            }
+
+            sb.Append($"</DrawingGroup>");
+
+            return sb.ToString();
+        }
+    }
+}
--- SvgToXaml/ViewModels/MainWindowViewModel.cs
+++ SvgToXaml/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Avalonia;
@@ -11,10 +9,7 @@ using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Threading;
 using ReactiveUI;
-using ShimSkiaSharp;
 using Svg.Skia;
-using SKPathArcSize = SkiaSharp.SKPathArcSize;
-using SKPathDirection = SkiaSharp.SKPathDirection;
 
 namespace SvgToXaml.ViewModels
 {
@@ -97,6 +92,11 @@ namespace SvgToXaml.ViewModels
 
             ClipboardCommand = ReactiveCommand.CreateFromTask<string>(async format =>
             {
+                if (string.IsNullOrWhiteSpace(format))
+                {
+                    return;
+                }
+
                 var text = await Dispatcher.UIThread.InvokeAsync(async () =>
                 {
                     try
@@ -123,7 +123,7 @@ namespace SvgToXaml.ViewModels
                     // ignored
                 }
 
-                var xaml = await Task.Run(() => ToXaml(svg.Model));
+                var xaml = await Task.Run(() => SvgConverter.ToXaml(svg.Model));
 
                 await Dispatcher.UIThread.InvokeAsync(() =>
                 {
@@ -160,222 +160,9 @@ namespace SvgToXaml.ViewModels
             });
         }
 
-       private static void ToPathData(SKPath path, StringBuilder sb)
-       {
-            if (path.FillType != SKPathFillType.Winding)
-            {
-                sb.Append($"F1 ");
-            }
-
-            if (path.Commands is null)
-            {
-                return;
-            }
-
-            for (var index = 0; index < path.Commands.Count; index++)
-            {
-                var pathCommand = path.Commands[index];
-
-                switch (pathCommand)
-                {
-                    case MoveToPathCommand moveToPathCommand:
-                    {
-                        var x = moveToPathCommand.X;
-                        var y = moveToPathCommand.Y;
-                        sb.Append($"{(index > 0 ? " " : "")}M{x.ToString(CultureInfo.InvariantCulture)},{y.ToString(CultureInfo.InvariantCulture)}");
-                    }
-                        break;
-                    case LineToPathCommand lineToPathCommand:
-                    {
-                        var x = lineToPathCommand.X;
-                        var y = lineToPathCommand.Y;
-                        sb.Append($"{(index > 0 ? " " : "")}L{x.ToString(CultureInfo.InvariantCulture)},{y.ToString(CultureInfo.InvariantCulture)}");
-                    }
-                        break;
-                    case ArcToPathCommand arcToPathCommand:
-                    {
-                        var rx = arcToPathCommand.Rx;
-                        var ry = arcToPathCommand.Ry;
-                        var xAxisRotate = arcToPathCommand.XAxisRotate;
-                        var largeArc = arcToPathCommand.LargeArc.ToSKPathArcSize();
-                        var sweep = arcToPathCommand.Sweep.ToSKPathDirection();
-                        var x = arcToPathCommand.X;
-                        var y = arcToPathCommand.Y;
-                        sb.Append($"{(index > 0 ? " " : "")}A{rx.ToString(CultureInfo.InvariantCulture)},{ry.ToString(CultureInfo.InvariantCulture)} {xAxisRotate.ToString(CultureInfo.InvariantCulture)}, {(largeArc == SKPathArcSize.Large ? "1" : "0")}, {(sweep == SKPathDirection.Clockwise ? "1" : "0")}, {x.ToString(CultureInfo.InvariantCulture)},{y.ToString(CultureInfo.InvariantCulture)});");
-                    }
-                        break;
-                    case QuadToPathCommand quadToPathCommand:
-                    {
-                        var x0 = quadToPathCommand.X0;
-                        var y0 = quadToPathCommand.Y0;
-                        var x1 = quadToPathCommand.X1;
-                        var y1 = quadToPathCommand.Y1;
-                        sb.Append($"{(index > 0 ? " " : "")}Q{x0.ToString(CultureInfo.InvariantCulture)},{y0.ToString(CultureInfo.InvariantCulture)} {x1.ToString(CultureInfo.InvariantCulture)},{y1.ToString(CultureInfo.InvariantCulture)}");
-                    }
-                        break;
-                    case CubicToPathCommand cubicToPathCommand:
-                    {
-                        var x0 = cubicToPathCommand.X0;
-                        var y0 = cubicToPathCommand.Y0;
-                        var x1 = cubicToPathCommand.X1;
-                        var y1 = cubicToPathCommand.Y1;
-                        var x2 = cubicToPathCommand.X2;
-                        var y2 = cubicToPathCommand.Y2;
-                        sb.Append($"{(index > 0 ? " " : "")}C{x0.ToString(CultureInfo.InvariantCulture)},{y0.ToString(CultureInfo.InvariantCulture)} {x1.ToString(CultureInfo.InvariantCulture)},{y1.ToString(CultureInfo.InvariantCulture)} {x2.ToString(CultureInfo.InvariantCulture)},{y2.ToString(CultureInfo.InvariantCulture)}");
-                    }
-                        break;
-                    case ClosePathCommand _:
-                    {
-                        sb.Append($" Z");
-                    }
-                        break;
-                    case AddRectPathCommand addRectPathCommand:
-                    {
-                        // TODO:
-                    }
-                        break;
-                    case AddRoundRectPathCommand addRoundRectPathCommand:
-                    {
-                        // TODO:
-                    }
-                        break;
-                    case AddOvalPathCommand addOvalPathCommand:
-                    {
-                        // TODO:
-                    }
-                        break;
-                    case AddCirclePathCommand addCirclePathCommand:
-                    {
-                        // TODO:
-                    }
-                        break;
-                    case AddPolyPathCommand addPolyPathCommand:
-                    {
-                        if (addPolyPathCommand.Points is not null && addPolyPathCommand.Points.Count >= 2)
-                        {
-                            var mx = addPolyPathCommand.Points[0].X;
-                            var my = addPolyPathCommand.Points[0].Y;
-                            
-                            sb.Append($"{(index > 0 ? " " : "")}M{mx.ToString(CultureInfo.InvariantCulture)},{my.ToString(CultureInfo.InvariantCulture)}");
-
-                            for (int i = 1; i < addPolyPathCommand.Points.Count; i++)
-                            {
-                                var lx = addPolyPathCommand.Points[i].X;
-                                var ly = addPolyPathCommand.Points[i].Y;
-                                sb.Append($"{(index > 0 ? " " : "")}L{lx.ToString(CultureInfo.InvariantCulture)},{ly.ToString(CultureInfo.InvariantCulture)}");
-                            }
-
-                            if (addPolyPathCommand.Close)
-                            {
-                                sb.Append($" Z");
-                            }
-                        }
-                    }
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
-
-        private static string ToXaml(SKPicture? model)
-        {
-            var sb = new StringBuilder();
-
-            sb.Append($"<DrawingGroup>\r\n");
-
-            if (model?.Commands is { })
-            {
-                foreach (var canvasCommand in model.Commands)
-                {
-                    switch (canvasCommand)
-                    {
-                        case ClipPathCanvasCommand clipPathCanvasCommand:
-                        {
-                            // TODO:
-                        }
-                            break;
-                        case ClipRectCanvasCommand clipRectCanvasCommand:
-                        {
-                            // TODO:;
-                        }
-                            break;
-                        case SaveCanvasCommand _:
-                        {
-                            // TODO:
-                        }
-                            break;
-                        case RestoreCanvasCommand _:
-                        {
-                            // TODO:
-                        }
-                            break;
-                        case SetMatrixCanvasCommand setMatrixCanvasCommand:
-                        {
-                            // TODO:
-                        }
-                            break;
-                        case SaveLayerCanvasCommand saveLayerCanvasCommand:
-                        {
-                            // TODO:
-                        }
-                            break;
-                        case DrawImageCanvasCommand drawImageCanvasCommand:
-                        {
-                            // TODO:
-                        }
-                            break;
-                        case DrawPathCanvasCommand drawPathCanvasCommand:
-                        {
-                            if (drawPathCanvasCommand.Path is { } && drawPathCanvasCommand.Paint is { })
-                            {
-                                var sbPath = new StringBuilder();
-
-                                ToPathData(drawPathCanvasCommand.Path, sbPath);
-
-                                var data = sbPath.ToString();
-
-                                if (drawPathCanvasCommand.Paint.Style == SKPaintStyle.Fill)
-                                {
-                                    if (drawPathCanvasCommand.Paint.Shader is ColorShader colorShader)
-                                    {
-                                        var brush =
-                                            $"#{colorShader.Color.Alpha:X2}{colorShader.Color.Red:X2}{colorShader.Color.Green:X2}{colorShader.Color.Blue:X2}";
-                                        sb.Append($"  <GeometryDrawing Brush=\"{brush}\" Geometry=\"{data}\"/>\r\n");
-                                    }
-                                }
-                            }
-                        }
-                            break;
-                        case DrawTextBlobCanvasCommand drawPositionedTextCanvasCommand:
-                        {
-                            // TODO:
-                        }
-                            break;
-                        case DrawTextCanvasCommand drawTextCanvasCommand:
-                        {
-                            // TODO:
-                        }
-                            break;
-                        case DrawTextOnPathCanvasCommand drawTextOnPathCanvasCommand:
-                        {
-                            // TODO:
-                        }
-                            break;
-                        default:
-                            break;
-                    }
-                }
-            }
-
-            sb.Append($"</DrawingGroup>");
-
-            return sb.ToString();
-        }
-
         private static async Task<string> ToXaml(FileItemViewModel fileItemViewModel)
         {
-            return await Task.Run(() => ToXaml(fileItemViewModel.Svg?.Model));
+            return await Task.Run(() => SvgConverter.ToXaml(fileItemViewModel.Svg?.Model));
         }
 
         public async void Drop(IEnumerable<string> paths)
