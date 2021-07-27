441db00b53f4d610c7a6c5c2641e97f679bf8a95
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-07-27 09:58:15 +0200

    Update SvgConverter.cs

--- SvgToXaml/SvgConverter.cs
+++ SvgToXaml/SvgConverter.cs
@@ -36,7 +36,7 @@ namespace SvgToXaml
                     }
                     case ArcToPathCommand(var rx, var ry, var xAxisRotate, var largeArc, var sweep, var x, var y):
                     {
-                        sb.Append($"{(index > 0 ? " " : "")}A{rx.ToString(CultureInfo.InvariantCulture)},{ry.ToString(CultureInfo.InvariantCulture)} {xAxisRotate.ToString(CultureInfo.InvariantCulture)} {(largeArc == SKPathArcSize.Large ? "1" : "0")} {(sweep == SKPathDirection.Clockwise ? "1" : "0")} {x.ToString(CultureInfo.InvariantCulture)},{y.ToString(CultureInfo.InvariantCulture)});");
+                        sb.Append($"{(index > 0 ? " " : "")}A{rx.ToString(CultureInfo.InvariantCulture)},{ry.ToString(CultureInfo.InvariantCulture)} {xAxisRotate.ToString(CultureInfo.InvariantCulture)} {(largeArc == SKPathArcSize.Large ? "1" : "0")} {(sweep == SKPathDirection.Clockwise ? "1" : "0")} {x.ToString(CultureInfo.InvariantCulture)},{y.ToString(CultureInfo.InvariantCulture)}");
                         break;
                     }
                     case QuadToPathCommand(var x0, var y0, var x1, var y1):
@@ -101,15 +101,90 @@ namespace SvgToXaml
             }
         }
 
-        public static string ToXaml(SKPicture? model)
+        public static string ToHexColor(SKColor skColor, string indent = "")
+        {
+            return $"{indent}#{skColor.Alpha:X2}{skColor.Red:X2}{skColor.Green:X2}{skColor.Blue:X2}";
+        }
+
+        public static string ToPoint(SKPoint skPoint)
+        {
+            return $"{skPoint.X.ToString(CultureInfo.InvariantCulture)},{skPoint.Y.ToString(CultureInfo.InvariantCulture)}";
+        }
+
+        public static string ToGradientSpreadMethod(SKShaderTileMode shaderTileMode)
+        {
+            switch (shaderTileMode)
+            {
+                default:
+                case SKShaderTileMode.Clamp:
+                    return "Pad";
+
+                case SKShaderTileMode.Repeat:
+                    return "Repeat";
+
+                case SKShaderTileMode.Mirror:
+                    return "Reflect";
+            }
+        }
+
+        public static string ToBrush(SKShader skShader, string indent = "")
+        {
+            if (skShader is ColorShader colorShader)
+            {
+                var brush = $"{indent}<SolidColorBrush Color=\"{ToHexColor(colorShader.Color)}\"/>\r\n";
+                return brush;
+            }
+
+            if (skShader is LinearGradientShader linearGradientShader)
+            {
+                var brush = "";
+
+                if (linearGradientShader.LocalMatrix is { })
+                {
+                    // TODO:
+                }
+
+                brush += $"{indent}<LinearGradientBrush StartPoint=\"{ToPoint(linearGradientShader.Start)}\" EndPoint=\"{ToPoint(linearGradientShader.End)}\" SpreadMethod=\"{ToGradientSpreadMethod(linearGradientShader.Mode)}\">\r\n";
+                brush += $"{indent}  <LinearGradientBrush.GradientStops>\r\n";
+
+                if (linearGradientShader.Colors is { } && linearGradientShader.ColorPos is { })
+                {
+                    for (int i = 0; i < linearGradientShader.Colors.Length; i++)
+                    {
+                        var color = ToHexColor(linearGradientShader.Colors[i]);
+                        var offset = linearGradientShader.ColorPos[i].ToString(CultureInfo.InvariantCulture);
+                        brush += $"{indent}    <GradientStop Offset=\"{offset}\" Color=\"{color}\"/>\r\n";
+                    }
+                }
+
+                brush += $"{indent}  </LinearGradientBrush.GradientStops>\r\n";
+                brush += $"{indent}</LinearGradientBrush>\r\n";
+
+                return brush;
+            }
+
+            if (skShader is TwoPointConicalGradientShader twoPointConicalGradientShader)
+            {
+                // TODO:
+            }
+
+            if (skShader is PictureShader pictureShader)
+            {
+                // TODO:
+            }
+
+            return "";
+        }
+        
+        public static string ToXaml(SKPicture? skPicture)
         {
             var sb = new StringBuilder();
 
             sb.Append($"<DrawingGroup>\r\n");
 
-            if (model?.Commands is { })
+            if (skPicture?.Commands is { })
             {
-                foreach (var canvasCommand in model.Commands)
+                foreach (var canvasCommand in skPicture.Commands)
                 {
                     switch (canvasCommand)
                     {
@@ -153,18 +228,50 @@ namespace SvgToXaml
                             var sbPath = new StringBuilder();
 
                             ToPathData(skPath, sbPath);
-
-                            // var data = Svg.Skia.SkiaModelExtensions.ToSKPath(skPath).ToSvgPathData();
                             var data = sbPath.ToString();
+                            //var data = Svg.Skia.SkiaModelExtensions.ToSKPath(skPath).ToSvgPathData();
 
-                            if (skPaint.Style == SKPaintStyle.Fill)
+                            var brush = default(string);
+                            var pen = default(string);
+
+                            if (skPaint.Style == SKPaintStyle.Fill || skPaint.Style == SKPaintStyle.StrokeAndFill)
                             {
-                                if (skPaint.Shader is ColorShader colorShader)
+                                if (skPaint.Shader is { })
                                 {
-                                    var brush = $"#{colorShader.Color.Alpha:X2}{colorShader.Color.Red:X2}{colorShader.Color.Green:X2}{colorShader.Color.Blue:X2}";
-                                    sb.Append($"  <GeometryDrawing Brush=\"{brush}\" Geometry=\"{data}\"/>\r\n");
+                                    brush = ToBrush(skPaint.Shader, "      ");
                                 }
                             }
+
+                            if (skPaint.Style == SKPaintStyle.Stroke || skPaint.Style == SKPaintStyle.StrokeAndFill)
+                            {
+                                if (skPaint.Shader is { })
+                                {
+                                    pen = $"      <Pen Thickness=\"{skPaint.StrokeWidth.ToString(CultureInfo.InvariantCulture)}\">\r\n" +
+                                          $"        <Pen.Brush>\r\n" +
+                                          ToBrush(skPaint.Shader, "          ") +
+                                          $"        </Pen.Brush>\r\n" +
+                                          $"      </Pen>\r\n";
+                                }
+                            }
+
+                            sb.Append($"  <GeometryDrawing Geometry=\"{data}\">\r\n");
+
+                            if (brush is { })
+                            {
+                                sb.Append($"    <GeometryDrawing.Brush>\r\n");
+                                sb.Append($"{brush}");
+                                sb.Append($"    </GeometryDrawing.Brush>\r\n");
+                            }
+                            
+                            if (pen is { })
+                            {
+                                sb.Append($"    <GeometryDrawing.Pen>\r\n");
+                                sb.Append($"{pen}");
+                                sb.Append($"    </GeometryDrawing.Pen>\r\n");
+                            }
+
+                            sb.Append($"  </GeometryDrawing>\r\n");
+
                             break;
                         }
                         case DrawTextBlobCanvasCommand(var skTextBlob, var f, var y, var skPaint):
