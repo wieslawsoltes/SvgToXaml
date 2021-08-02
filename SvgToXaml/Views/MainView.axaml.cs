4a88e7521275932972ab540fa2630d197d61718c
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-08-02 09:10:20 +0200

    Add MainView

---
+++ SvgToXaml/Views/MainView.axaml.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SvgToXaml.Views
+{
+    public class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
+
