a205dfe9b2c2dc664abc12c77d7ecf965397a744
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-08-04 12:23:13 +0200

    Create PreviewWindow.axaml.cs

---
+++ SvgToXaml/Views/PreviewWindow.axaml.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SvgToXaml.Views
+{
+    public class PreviewWindow : Window
+    {
+        public PreviewWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
+
