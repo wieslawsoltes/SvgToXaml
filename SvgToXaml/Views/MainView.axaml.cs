b98d6f3eadf4ea0147138f7caca0fff1dd1ff8ba
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-08-04 13:16:46 +0200

    Handle DoubleTapped

--- SvgToXaml/Views/MainView.axaml.cs
+++ SvgToXaml/Views/MainView.axaml.cs
@@ -1,6 +1,8 @@
-﻿using Avalonia;
+﻿using System.Reactive;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
+using SvgToXaml.ViewModels;
 
 namespace SvgToXaml.Views
 {
@@ -14,6 +16,17 @@ namespace SvgToXaml.Views
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private void Panel_OnDoubleTapped(object? sender, TappedEventArgs e)
+        {
+            if (sender is Panel panel && panel.DataContext is FileItemViewModel fileItemViewModel)
+            {
+                if (fileItemViewModel.PreviewCommand.CanExecute(Unit.Default))
+                {
+                    fileItemViewModel.PreviewCommand.Execute(Unit.Default);
+                }
+            }
         }
     }
 }
