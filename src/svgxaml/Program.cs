232f98ef4d3952597de7bce6879fa172e795b488
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-01-11 20:02:55 +0100

    Update Program.cs

--- src/svgxaml/Program.cs
+++ src/svgxaml/Program.cs
@@ -6,134 +6,126 @@ using System.Linq;
 using Svg.Skia;
 using SvgToXamlConverter.Model;
 
-namespace svgxaml;
-
-class Program
+if (args.Length != 1 && args.Length != 2)
 {
-    static void GetFiles(string path, List<string> paths)
+    Console.WriteLine("Usage: svgxaml <InputPath> [OutputPath]");
+    return;
+}
+
+try
+{
+    var inputPath = args[0];
+
+    if (File.GetAttributes(inputPath).HasFlag(FileAttributes.Directory))
     {
-        if (File.GetAttributes(path).HasFlag(FileAttributes.Directory))
+        var paths = new List<string>();
+
+        GetFiles(inputPath, paths);
+
+        paths.Sort();
+
+        if (paths.Count == 0)
         {
-            var svgPaths = Directory.EnumerateFiles(path, "*.svg", new EnumerationOptions {RecurseSubdirectories = true});
-            var svgzPaths = Directory.EnumerateFiles(path, "*.svgz", new EnumerationOptions {RecurseSubdirectories = true});
-            svgPaths.ToList().ForEach(x => GetFiles(x, paths));
-            svgzPaths.ToList().ForEach(x => GetFiles(x, paths));
             return;
         }
 
-        var extension = Path.GetExtension(path);
-        switch (extension.ToLower())
+        var converter = new SvgToXamlConverter.SvgToXamlConverter()
         {
-            case ".svg":
-            case ".svgz":
-                paths.Add(path);
-                break;
-        }
-    }
+            UseCompatMode = false,
+            ReuseExistingResources = false,
+            Resources = null
+        };
 
-    static Stream LoadFromStream(Stream stream, string name)
-    {
-        var extension = Path.GetExtension(name);
-        var memoryStream = new MemoryStream();
-        
-        if (extension == "svgz")
-        { 
-            using var gzipStream = new GZipStream(stream, CompressionMode.Decompress); 
-            gzipStream.CopyTo(memoryStream);
-        }
-        else
+        var xaml = converter.ToXamlStyles(paths.Select(x =>
         {
-            stream.CopyTo(memoryStream);
-        }
+            using var stream = File.OpenRead(x);
+            var ms = LoadFromStream(stream, x);
+            using var reader = new StreamReader(ms);
+            var content = reader.ReadToEnd();
+            return new InputItem(Path.GetFileName(x), content);
+        }).ToList());
 
-        memoryStream.Position = 0;
-        return memoryStream;
-    }
-
-    static void Main(string[] args)
-    {
-        if (args.Length != 1 && args.Length != 2)
+        if (args.Length == 1)
         {
-            Console.WriteLine("Usage: svgxaml <InputPath> [OutputPath]");
+            Console.WriteLine(converter.Format(xaml));
             return;
         }
 
-        try
+        if (args.Length == 2)
         {
-            var inputPath = args[0];
-
-            if (File.GetAttributes(inputPath).HasFlag(FileAttributes.Directory))
-            {
-                var paths = new List<string>();
-
-                GetFiles(inputPath, paths);
-
-                paths.Sort();
-
-                if (paths.Count == 0)
-                {
-                    return;
-                }
-
-                var converter = new SvgToXamlConverter.SvgToXamlConverter()
-                {
-                    UseCompatMode = false,
-                    ReuseExistingResources = false,
-                    Resources = null
-                };
-
-                var xaml = converter.ToXamlStyles(paths.Select(x =>
-                {
-                    using var stream = File.OpenRead(x);
-                    var ms = LoadFromStream(stream, x);
-                    using var reader = new StreamReader(ms);
-                    var content = reader.ReadToEnd();
-                    return new InputItem(Path.GetFileName(x), content);
-                }).ToList());
-
-                if (args.Length == 1)
-                {
-                    Console.WriteLine(converter.Format(xaml));
-                    return;
-                }
-
-                if (args.Length == 2)
-                {
-                    var outputPath = args[1];
-                    File.WriteAllText(outputPath, converter.Format(xaml));
-                }
-            }
-            else
-            {
-                var converter = new SvgToXamlConverter.SvgToXamlConverter()
-                {
-                    UseCompatMode = false,
-                    ReuseExistingResources = false,
-                    Resources = null
-                };
-
-                var skSvg = new SKSvg();
-                skSvg.Load(inputPath);
-
-                var xaml = converter.ToXamlImage(skSvg.Model);
-
-                if (args.Length == 1)
-                {
-                    Console.WriteLine(converter.Format(xaml));
-                    return;
-                }
-
-                if (args.Length == 2)
-                {
-                    var outputPath = args[1];
-                    File.WriteAllText(outputPath, converter.Format(xaml));
-                }
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"{ex.Message}");
-            Console.WriteLine($"{ex.StackTrace}");
+            var outputPath = args[1];
+            File.WriteAllText(outputPath, converter.Format(xaml));
         }
     }
+    else
+    {
+        var converter = new SvgToXamlConverter.SvgToXamlConverter()
+        {
+            UseCompatMode = false,
+            ReuseExistingResources = false,
+            Resources = null
+        };
+
+        var skSvg = new SKSvg();
+        skSvg.Load(inputPath);
+
+        var xaml = converter.ToXamlImage(skSvg.Model);
+
+        if (args.Length == 1)
+        {
+            Console.WriteLine(converter.Format(xaml));
+            return;
+        }
+
+        if (args.Length == 2)
+        {
+            var outputPath = args[1];
+            File.WriteAllText(outputPath, converter.Format(xaml));
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"{ex.Message}");
+    Console.WriteLine($"{ex.StackTrace}");
+}
+
+static void GetFiles(string path, List<string> paths)
+{
+    if (File.GetAttributes(path).HasFlag(FileAttributes.Directory))
+    {
+        var svgPaths = Directory.EnumerateFiles(path, "*.svg", new EnumerationOptions {RecurseSubdirectories = true});
+        var svgzPaths = Directory.EnumerateFiles(path, "*.svgz", new EnumerationOptions {RecurseSubdirectories = true});
+        svgPaths.ToList().ForEach(x => GetFiles(x, paths));
+        svgzPaths.ToList().ForEach(x => GetFiles(x, paths));
+        return;
+    }
+
+    var extension = Path.GetExtension(path);
+    switch (extension.ToLower())
+    {
+        case ".svg":
+        case ".svgz":
+            paths.Add(path);
+            break;
+    }
+}
+
+static Stream LoadFromStream(Stream stream, string name)
+{
+    var extension = Path.GetExtension(name);
+    var memoryStream = new MemoryStream();
+    
+    if (extension == "svgz")
+    { 
+        using var gzipStream = new GZipStream(stream, CompressionMode.Decompress); 
+        gzipStream.CopyTo(memoryStream);
+    }
+    else
+    {
+        stream.CopyTo(memoryStream);
+    }
+
+    memoryStream.Position = 0;
+    return memoryStream;
 }
